36a4543eb4846ef1bd1a3112f63284e3f5bcd4cd
Kye <kye@apacmediasolutions.com>
2023-08-29 10:01:22 -0400

    base

--- aot/main.py
+++ aot/main.py
@@ -1,3 +1,14 @@
 from swarms import OpenAI
 
 
+class AoT:
+    def __init__(self,
+                 llm = None,
+                 task: str = None):
+        super(AoT, self).__init__()
+        self.llm = llm
+        self.task = task
+    
+    def run(self):
+        self.llm.run(self.task)
+    
