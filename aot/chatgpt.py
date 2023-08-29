13aeebf71bef443b49838c4b33b116d4c443f997
Kye <kye@apacmediasolutions.com>
2023-08-29 15:53:45 -0400

    solutions

--- aot/chatgpt.py
+++ aot/chatgpt.py
@@ -126,14 +126,17 @@ class OpenAILanguageModel:
             else:
                 state_text = state
             
-            prompt = f"""You're an TreeofThoughts, an superintelligent AI model devoted to helping Humans by any means necessary. You're purpose is to generate a series of solutions to comply with the user's instructions, you must generate solutions on the basis of determining the most reliable solution in the shortest amount of time, while taking rejected solutions into account and learning from them. 
+            prompt = f"""
+            Generate a series of solutions to comply with the user's instructions, 
+            you must generate solutions on the basis of determining the most reliable solution in the shortest amount of time, 
+            while taking rejected solutions into account and learning from them. 
             Considering the reasoning provided:\n\n
             ###'{state_text}'\n\n###
             Devise the best possible solution for the task: {initial_prompt}, Here are evaluated solutions that were rejected: 
             ###{rejected_solutions}###, 
             complete the {initial_prompt} without making the same mistakes you did with the evaluated rejected solutions. Be simple. Be direct. Provide intuitive solutions as soon as you think of them."""
             answer = self.generate_text(prompt, 1)
-            print(f'Answerrrrrr {answer}')
+            print(f'Generated Solution Summary {answer}')
             return answer
         except Exception as e:
             logger.error(f"Error in generate_solutions: {e}")
